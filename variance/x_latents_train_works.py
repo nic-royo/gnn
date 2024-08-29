7d55750b053a102830edb2dbb4a8590e34e61255
Nicolas Obregon Royo <niob@itu.dk>
2024-08-29 10:25:30 +0200

    updates, training for embeddings now works

--- variance/x_latents_train_works.py
+++ variance/x_latents_train_works.py
@@ -1,4 +1,4 @@
-# 'minecraft water' plots code for doing the training and visualization of the embeddings, only does 50 random nodes, for now
+# Code for making water plots, only training, trains on target node types, makes csv files for embeddings
 import torch
 from torch_geometric.utils import degree
 import torch.nn as nn
@@ -68,7 +68,7 @@ class RGCN(nn.Module):
             x_latents.append(x.detach())
         x = self.convs[-1](x, edge_index, edge_type)
         return x, x_latents
-
+ 
 class pairnormRGCN(nn.Module):
     def __init__(self, metadata, hidden_dim, out_dim, num_bases, dropout=0.8, num_layers=2):
         super(pairnormRGCN, self).__init__()
@@ -126,122 +126,6 @@ class pairnormRGCN(nn.Module):
         x = self.pairnorms[-1](x)  # do PairNorm after the final RGCN layer
         return x, x_latents
 
-class resRGCN(nn.Module):
-    def __init__(self, metadata, hidden_dim, out_dim, num_bases, dropout=0.8, num_layers=2):
-        super(resRGCN, self).__init__()
-        self.dropout = nn.Dropout(dropout)
-        self.relu = nn.ReLU()
-        
-        self.node_types, self.edge_types = metadata
-        
-        self.linear_projections = nn.ModuleDict()
-        
-        self.convs = nn.ModuleList()
-        self.convs.append(RGCNConv(hidden_dim, hidden_dim, num_relations=len(self.edge_types), num_bases=num_bases))
-        for _ in range(num_layers - 2):
-            self.convs.append(RGCNConv(hidden_dim, hidden_dim, num_relations=len(self.edge_types), num_bases=num_bases))
-        self.convs.append(RGCNConv(hidden_dim, out_dim, num_relations=len(self.edge_types), num_bases=num_bases))
-
-    def create_linear_projections(self, x_dict):
-        device = next(self.parameters()).device
-        for node_type, x in x_dict.items():
-            if node_type not in self.linear_projections:
-                in_dim = x.size(1)
-                self.linear_projections[node_type] = nn.Linear(in_dim, self.convs[0].in_channels).to(device)
-
-    def forward(self, x_dict, data):
-        device = next(self.parameters()).device
-
-        # Create linear projections if they don't exist
-        self.create_linear_projections(x_dict)
-
-        # Linear projections for each node type
-        x_dict = {node_type: self.dropout(self.relu(self.linear_projections[node_type](x.to(device))))
-                for node_type, x in x_dict.items()}
-
-        # Combine all node features
-        x = torch.cat([x_dict[node_type] for node_type in self.node_types])
-        
-        index_data = data.to_homogeneous()
-        edge_index = index_data.edge_index
-        edge_type = index_data.edge_type
-        x_latents = [x] 
-        
-        # Apply RGCN layers
-        for i, conv in enumerate(self.convs[:-1]):
-            x_ = self.dropout(self.relu(conv(x, edge_index, edge_type)))
-            x_latents.append(x)
-            
-            # Residual connection - no bias
-            if i == 0:
-                x = x_
-            else:
-                x = x + x_
-
-        out = self.convs[-1](x, edge_index, edge_type)
-        
-        return out, x_latents
-
-class ggRGCN(nn.Module):
-    def __init__(self, metadata, hidden_dim, out_dim, num_bases, dropout=0.5, num_layers=2, p=2):
-        super(ggRGCN, self).__init__()
-        self.dropout = nn.Dropout(dropout)
-        self.relu = nn.ReLU()
-        self.node_types, self.edge_types = metadata
-        self.num_layers = num_layers
-        self.p = p
-    
-        self.linear_projections_in = nn.ModuleDict()
-        
-        self.rgcn_layers = nn.ModuleList()
-        self.gg_layers = nn.ModuleList()
-        for _ in range(num_layers):
-            self.rgcn_layers.append(RGCNConv(hidden_dim, hidden_dim, num_relations=len(self.edge_types), num_bases=num_bases))
-            self.gg_layers.append(RGCNConv(hidden_dim, hidden_dim, num_relations=len(self.edge_types), num_bases=num_bases))
-        
-        #decoder layer
-        self.linear_output=nn.Linear(hidden_dim, out_dim)
-
-    def create_linear_projections(self, x_dict):
-        device = next(self.parameters()).device
-        for node_type, x in x_dict.items():
-            if node_type not in self.linear_projections_in:
-                in_dim = x.size(1)
-                self.linear_projections_in[node_type] = nn.Linear(in_dim, self.rgcn_layers[0].in_channels).to(device)
-
-    def forward(self, x_dict, data):
-        device = next(self.parameters()).device
-
-        # Create linear projections if they don't exist
-        self.create_linear_projections(x_dict)
-
-        # Linear projections for each node type
-        x_dict = {node_type: self.dropout(self.relu(self.linear_projections_in[node_type](x.to(device))))
-                for node_type, x in x_dict.items()}
-
-        # Combine all node features
-        x = torch.cat([x_dict[node_type] for node_type in self.node_types])
-        
-        index_data = data.to_homogeneous()
-        edge_index = index_data.edge_index
-        edge_type = index_data.edge_type
-        x_latents = [x] 
-        
-        # GG methods
-        for i in range(self.num_layers):
-            x_ = torch.relu(self.rgcn_layers[i](x, edge_index, edge_type))
-            
-            # Estimate of tau ind order to calculate correct tau
-            x_hat = torch.relu(self.gg_layers[i](x, edge_index, edge_type))
-            tau = torch.tanh(scatter((torch.abs(x[edge_index[0]] - x[edge_index[1]]) ** self.p).squeeze(-1),
-                                 edge_index[0], 0, dim_size=x.size(0), reduce='mean'))
-            x = (1 - tau) * x + tau * x_
-            x_latents.append(x)            
-        
-        x = self.dropout(x)    
-        x = self.linear_output(x)
-        return x, x_latents
-
 def load_dataset(dataset_name, root='/tmp/'):
     if dataset_name == 'imdb':
         return IMDB(root=root + 'IMDB')[0]
@@ -265,7 +149,7 @@ def train(model, optimizer, data, target_node_type):
     model.train()
     optimizer.zero_grad()
     out, _ = model(data.x_dict, data)
-    target_out = out[target_node_type]
+    target_out = out[:data[target_node_type].num_nodes]
     loss = F.cross_entropy(target_out[data[target_node_type].train_mask],
                            data[target_node_type].y[data[target_node_type].train_mask])
     loss.backward()
@@ -276,7 +160,7 @@ def val(model, data, target_node_type):
     model.eval()
     with torch.no_grad():
         out, _ = model(data.x_dict, data)
-        target_out = out[target_node_type]
+        target_out = out[:data[target_node_type].num_nodes]
         pred = target_out.argmax(dim=1)
         correct = pred[data[target_node_type].val_mask] == data[target_node_type].y[data[target_node_type].val_mask]
         acc = int(correct.sum()) / int(data[target_node_type].val_mask.sum())
@@ -284,8 +168,8 @@ def val(model, data, target_node_type):
                       pred[data[target_node_type].val_mask].cpu().numpy(), average='micro')
     return acc, f1
 
-def train_model(model, data, target_node_type, epochs=10, learning_rate=0.01, weight_decay=5e-4):
-    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
+def train_model(model, data, target_node_type, epochs=10, learning_rate=0.01):
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
     best_f1 = 0
     best_model = None
@@ -294,7 +178,7 @@ def train_model(model, data, target_node_type, epochs=10, learning_rate=0.01, we
         loss = train(model, optimizer, data, target_node_type)
         acc, f1 = val(model, data, target_node_type)
 
-        print(f'Epoch: {epoch+1:03d}, Loss: {loss:.4f}, Val Acc: {acc:.4f}, Val F1-Score: {f1:.4f}')
+        #print(f'Epoch: {epoch+1:03d}, Loss: {loss:.4f}, Val Acc: {acc:.4f}, Val F1-Score: {f1:.4f}')
 
         if f1 > best_f1:
             best_f1 = f1
@@ -302,95 +186,78 @@ def train_model(model, data, target_node_type, epochs=10, learning_rate=0.01, we
 
     # Load the best model
     model.load_state_dict(best_model)
-    return model
+
 
 def main(args):
-    # Load dataset
     data = load_dataset(args.dataset, root=args.data_root)
 
-    # Get target node type and number of classes
     target_node_type = get_target_node_type(args.dataset)
     num_classes = get_num_classes(data, target_node_type)
 
-    # Set device
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     data = data.to(device)
-
-    pyg.seed_everything(seed=42)
 
     # Create model
     if args.model_type == 'pnrgcn':
         model = pairnormRGCN(data.metadata(), args.hidden_dim, num_classes,
-                             args.num_bases, num_layers=args.num_hops).to(device)
+                             args.num_bases, dropout=args.dropout, num_layers=args.num_hops).to(device)
     elif args.model_type == 'rgcn':
         model = RGCN(data.metadata(), args.hidden_dim, num_classes,
-                     args.num_bases, num_layers=args.num_hops).to(device)
+                     args.num_bases, dropout=args.dropout, num_layers=args.num_hops).to(device)
     else:
         print("Model not found")
         return  # Exit the function if the model is not found
-    
+
     print(model)
 
+    # Training the model
     print("Training the model...")
-    model = train_model(model, data, target_node_type, epochs=args.epochs, learning_rate=args.learning_rate, weight_decay=args.weight_decay)
+    train_model(model, data, target_node_type, epochs=args.epochs, learning_rate=args.learning_rate)
 
-    print("Generating node embeddings...")
-    model.eval()
-    with torch.no_grad():
-        _, x_latents = model(data.x_dict, data)
-    
-    # Ensure x_latents is a dictionary mapping node types to their embeddings
-    x_latents_dict = {node_type: x_latent for node_type, x_latent in x_latents.items()}
-    
-    if args.node_types_option == 'all_node_types':
-        pass  # Keep all node types
-    elif args.node_types_option == 'target_node_type':
-        x_latents_dict = {target_node_type: x_latents_dict[target_node_type]}
-    else:
-        raise ValueError(f"Invalid node_types_option: {args.node_types_option}")
+    # After training, get the embeddings
+    pyg.seed_everything(250)
+    _, x_latents = model(data.x_dict, data)
 
     # Conditionally normalize the embeddings
     if args.embedding_option == 'normalize':
-        x_latents_dict = {node_type: F.normalize(x_latent, p=2, dim=1) for node_type, x_latent in x_latents_dict.items()}
-    
-    # Convert embeddings to numpy arrays
-    x_latents_np = {node_type: x_latent.detach().cpu().numpy() for node_type, x_latent in x_latents_dict.items()}
+        x_latents = [F.normalize(x_latent, p=2, dim=1) for x_latent in x_latents]
 
+    x_latents_np = [x_latent.detach().cpu().numpy() for x_latent in x_latents]
+
+    # Create the output directory if it doesn't exist
+    output_dir = os.path.join('results', 'train_embeddings')
+    os.makedirs(output_dir, exist_ok=True)
+
+    # Save embeddings only for the last layer (layer 32 when num_hops is 32)
+    layer = args.num_hops - 1  # Get the index of the last layer
+    start_idx = 0
     all_embeddings = []
-    all_node_indices = []
-
-    for node_type, embeddings in x_latents_np.items():
-        # Select node indices
-        num_nodes = data[node_type].num_nodes
-        if args.use_all_nodes:
-            node_indices = torch.arange(num_nodes)
-        else:
-            node_indices = torch.randperm(num_nodes)[:50]
-
-        # Get final layer embeddings
-        final_layer_embeddings = embeddings[node_indices]
-        
-        # Create DataFrame with node type and embeddings
-        df = pd.DataFrame(final_layer_embeddings, index=[f'{node_type}_Node_{i}' for i in node_indices])
-        df['Node_Type'] = node_type  # Add a column for node type
-        all_embeddings.append(df)
-        all_node_indices.append(node_indices)
     
-    # Combine all DataFrames into one
-    combined_df = pd.concat(all_embeddings, axis=0)
+    for node_type in data.node_types:
+        num_nodes = data[node_type].num_nodes
+        end_idx = start_idx + num_nodes
+        
+        node_embeddings = x_latents_np[layer][start_idx:end_idx]
+        node_types = [node_type] * num_nodes
+        
+        df = pd.DataFrame(node_embeddings)
+        df['Node_Type'] = node_types
+        all_embeddings.append(df)
+        
+        start_idx = end_idx
+    
+    # Concatenate all embeddings for the last layer
+    combined_df = pd.concat(all_embeddings, ignore_index=True)
+    
+    # Generate the file name
+    file_suffix = f"_{args.model_type}_{args.num_hops}_layer_all_nodes_{'norm' if args.embedding_option == 'normalize' else 'notnorm'}.csv"
+    csv_filename = os.path.join(output_dir, f"{args.dataset}_layer_{args.num_hops}{file_suffix}")
 
-    # Construct the output directory and filename
-    output_dir = f"results/train_embeddings/{args.dataset}_{args.model_type}"
-    os.makedirs(output_dir, exist_ok=True)  # Create directory if it does not exist
+    # Save the CSV file
+    combined_df.to_csv(csv_filename, index=False)
+    print(f"Saved embeddings for layer {args.num_hops} to {csv_filename}")
 
-    file_suffix = f"_{args.num_hops}_layer_all_nodes_{'norm' if args.embedding_option == 'normalize' else 'notnorm'}.csv"
-    csv_filename = os.path.join(output_dir, f"{args.dataset}_{args.model_type}{file_suffix}")
-
-    # Save the combined DataFrame to CSV
-    combined_df.to_csv(csv_filename)
-
-    print(f'All node embeddings saved to {csv_filename}')
-
+    print('Finished.')
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='RGCN for Heterogeneous Datasets')
@@ -399,12 +266,9 @@ if __name__ == "__main__":
     parser.add_argument('--hidden_dim', type=int, default=128, help='Hidden dimension size')
     parser.add_argument('--num_bases', type=int, default=None, help='Number of bases for RGCN')
     parser.add_argument('--dropout', type=float, default=0.5, help='Dropout rate')
-    parser.add_argument('--num_hops', type=int, default=32, help='Number of hops in RGCN')
+    parser.add_argument('--num_hops', type=int, default=8, help='Number of hops in RGCN')
     parser.add_argument('--embedding_option', type=str, required=True, choices=['normalize', 'no_normalize'], help='Option to either normalize or not normalize the embeddings')
-    parser.add_argument('--use_all_nodes', type=lambda x: x.lower() == 'true', default=True, help='Whether to use all nodes or a random sample of 50 nodes')
-    parser.add_argument('--node_types_option', type=str, default='all_node_types', choices=['all_node_types', 'target_node_type'], help='Whether to save embeddings for all node types or just the target node type')    
     parser.add_argument('--epochs', type=int, default=10, help='Number of training epochs')
-    parser.add_argument('--train', type=bool, required=True, help='Whether to train the model (True or False)')
     parser.add_argument('--learning_rate', type=float, default=0.01, help='Learning rate for training')
     parser.add_argument('--weight_decay', type=float, default=0.00005, help='Weight decay')
     parser.add_argument('--model_type', type=str, default='rgcn', choices=['rgcn', 'pnrgcn', 'resrgcn', 'ggrgcn'], help='Type of RGCN model to use')
